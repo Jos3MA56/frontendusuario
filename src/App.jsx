d14824e518d40d35c973323947dfb6cd9aaefb7e
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-06 01:01:37 -0600

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Routes, Route, Navigate, Router } from "react-router-dom";
+// App.jsx
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import MagicRequest from "./pages/MagicRequest.jsx";
 import Magic from "./pages/Magic.jsx";
-import Profile from "./pages/Profile.jsx";   // si ya lo tienes
-import Register from "./pages/Register.jsx"; // opcional
+import Profile from "./pages/Profile.jsx";
+import Register from "./pages/Register.jsx";
 import Home from "./pages/Home.jsx";
 import Header from "./components/Header.jsx";
 import "./styles.css";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
       <div className="header-spacer" />
       <Routes>
@@ -20,10 +21,8 @@ export default function App() {
         <Route path="/magic" element={<Magic />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-
