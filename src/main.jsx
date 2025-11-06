fa667de884c9494f000ecd3614e75aa86f09adcb
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-06 02:21:57 -0600

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { CartProvider } from "./context/CartContext.jsx";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import App from "./App.jsx";
+import { CartProvider } from "./context/CartContext.jsx"; // 🛒 Importa el contexto
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* Envolvemos TODA la app con el CartProvider */}
       <CartProvider>
-        <App />
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
